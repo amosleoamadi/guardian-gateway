a5ad4011ed498ac668be91747010dfd30177bccc
amosleoamadi <amadiamos146@gmail.com>
2026-04-07 12:30:38 +0100

    update

--- backend/server.js
+++ backend/server.js
@@ -45,12 +45,14 @@ mongoose
 // Email Transporter
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
 });
 
 transporter.verify((error, success) => {
