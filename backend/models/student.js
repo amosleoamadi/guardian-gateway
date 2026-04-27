45b3ae7c63180f33fbb7bc9109fb52107d31a67d
amosleoamadi <amadiamos146@gmail.com>
2026-04-27 23:10:47 +0100

    added name

--- backend/models/student.js
+++ backend/models/student.js
@@ -3,6 +3,10 @@ import mongoose from "mongoose";
 const studentSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   matricNo: { type: String, required: true },
+  name: {
+    type: String,
+    required: false,
+  },
   otp: String,
 });
 
